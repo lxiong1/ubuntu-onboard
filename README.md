2418f6d5c7ba95cc9fcfd506e624e08cdc93eccf
Lue Xiong <luexiong@pop-os.localdomain>
2020-03-04 22:04:28 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ubuntu-Onboard
+
+The purpose of the script is to pull down and install commonly used software for an Ubuntu or Ubuntu-based environment
---
+++ onboard.sh
@@ -0,0 +1,26 @@
+sudo apt update & sudo apt upgrade
+
+sudo apt install gdebi-core
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+yes | sudo gdebi google-chrome-stable_current_amd64.deb
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code
+
+yes | sudo apt install texlive-full
+
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
+yes | sudo apt install ./slack-desktop-*.deb
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+yes | sudo apt install docker-ce
+
+sudo apt install tlp tlp-rdw --no-install-recommends
